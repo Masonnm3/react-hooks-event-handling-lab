a3a063a823b1e9bf05c71a7fc25aa1b05bf65cb2
Michael Mason <m.masonn3@gmail.com>
2023-07-07 21:12:34 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,34 @@
-// Code EyesOnMe Component Here
+
+
+function EyesOnMe (){
+
+
+    const handleBlur = (event) =>
+    {
+        console.log('Hey! Eyes on me!')
+    }
+    const handleFocus =( event) =>
+    {
+        console.log('Good!')
+    }
+    
+    return( 
+        <>
+       <div onFocus ={handleFocus} onBlur={handleBlur}>
+            
+        <button> Eyes on me </button>
+        </div> 
+        </>
+
+)}
+
+
+
+
+
+
+
+
+
+
+export default EyesOnMe
